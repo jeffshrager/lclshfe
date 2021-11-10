81f47cf0eb189f862a2e76ca26449c6481e98f15
jeffshrager <jshrager@stanford.edu>
2021-11-09 17:41:54 -0800

    no message

--- bt1.py
+++ bt1.py
@@ -1,6 +1,5 @@
 # Todo:
 #    -- Where exact beam|jet match is tested with ==, replace with a more "perceptually" accurate model
-#    -- Calibate cycles (esp. operator_response_delay) to human reponse times (e.g., 1 cycle == 100ms or whatever)
 
 from scipy.stats import sem
 import numpy
@@ -34,7 +33,19 @@ n_crazy_ivans = 0 # !!!! YOU DON'T MEAN TO BE CHANGING THIS! IT'S THE P_ ABOVE!!
 # automation), which can be shifted in accord with these params:
 
 beam_shift_amount=0.01 # You may want to have more or less fine control of the beam vs. the stream's shiftiness
-operator_response_delay=0 # cycles before the operator can respond to a stream shift
+
+# Operator reponse delay combines noticing, attention shifting to button, decision delay, moving to the button, and pressing it
+# (Possibly also need attention shift into the display, but we're leaving that out bcs it can be arbitrarily large)
+noticing_delay = 1 # 100 ms
+decision_delay = 1 # 100 ms -- FFF incorporate differential switch time
+# attention shifting to button has to be computed from where we are and where the buttons are
+# current_eye_position = 0 
+# left_button_position = -2 # we're actually not gonna use these but just use a fixed shift time
+# right_button_position = +2
+switch_button_delay_per_cm = 1 # ms
+button_press_delay = 1 # ms
+button_distance = 0 # cm
+which_button_were_on = "<<" # or ">>"
 
 default_max_cycles=10000 # If the beam doesn't hit a wall before this, we cut the run off here.
 
@@ -51,7 +62,7 @@ global msg
 msg=""
 
 def run_stream(show_f=False):
-  global hits, misses, default_max_cycles, operator_response_delay, n_crazy_ivans, functional_acuity
+  global hits, misses, default_max_cycles, n_crazy_ivans, functional_acuity
   hits = 0
   misses = 0
   stream_pos = 0.0
@@ -70,11 +81,11 @@ def run_stream(show_f=False):
         msg=msg+"!!!"
         stream_pos=round(stream_pos+(crazy_ivan_shift_amount*porm()),4) 
         if allow_response_cycle==99999999999:
-          allow_response_cycle=cycle+operator_response_delay
+          allow_response_cycle=cycle+operator_response_delay(stream_pos,beam_pos)
     elif random.random() < p_stream_shift:
         stream_pos=round(stream_pos+(stream_shift_amount*porm()),4)
         if allow_response_cycle==99999999999:
-          allow_response_cycle=cycle+operator_response_delay
+          allow_response_cycle=cycle+operator_response_delay(stream_pos,beam_pos)
     update_stats(stream_pos,beam_pos)
     if show_f:
         showpos(stream_pos,beam_pos,show_f,cycle)
@@ -89,6 +100,23 @@ def run_stream(show_f=False):
         allow_response_cycle=99999999999
     cycle=cycle+1
 
+# ORD() uses the current eye position and button distances to decide
+# how many cycles it takes to hit the button, which is either short
+# (you're there already), or long (you're not), the longer using the
+# button distance to delay. It return an integer number of cycles to
+# wait before the input arrives.
+
+def operator_response_delay(stream_pos,beam_pos):
+  global switch_button_delay_per_cm, button_press_delay, button_distance, which_button_were_on, msg
+  way = which_way_do_we_need_to_shift(stream_pos, beam_pos)
+  if way == which_button_were_on:
+    msg=msg+"["+str(button_press_delay+decision_delay+noticing_delay)+"]"
+    return(button_press_delay+decision_delay+noticing_delay)
+  else:
+    which_button_were_on = way
+    msg=msg+"["+str((button_distance*switch_button_delay_per_cm)+button_press_delay+decision_delay+noticing_delay)+"]"
+    return((button_distance*switch_button_delay_per_cm)+button_press_delay+decision_delay+noticing_delay)
+  
 def update_stats(stream_pos, beam_pos):
   global physical_acuity, hits, misses
   delta = abs(beam_pos-stream_pos)
@@ -108,25 +136,33 @@ def porm():
 def trunc2(n):
   return(int(n*100.0)/100.0)
 
-# FFF This should use a model of visual UI-mediated visual acuity, rather than just exact operators.
+# FFF This should use a model of visual UI-mediated visual acuity,
+# rather than just exact operators.
 
 def track(stream_pos, beam_pos):
   global functional_acuity, msg
-  delta = abs(beam_pos-stream_pos)
-  if delta<functional_acuity:
+  which_way = which_way_do_we_need_to_shift(stream_pos, beam_pos)
+  if which_way == "none":
     msg=msg+"(FA)"
     return(beam_pos)
-  # ??? Since the acuity relates to the equality, I don't think we
-  # need it in the > and < tests -- they are protected by the ==
-  # test ???
-  elif beam_pos>stream_pos:
-    #print("<<"+str(beam_pos-beam_shift_amount))
+  elif which_way == "<<":
     msg=msg+"<<"
     return(beam_pos-beam_shift_amount)
   else:
-    #print(">>"+str(beam_pos+beam_shift_amount))
     msg=msg+">>"
     return(beam_pos+beam_shift_amount)
+
+# Used in various places, returns "<<", ">>", "none"
+
+def which_way_do_we_need_to_shift(stream_pos, beam_pos):
+  global functional_acuity, msg
+  delta = abs(beam_pos-stream_pos)
+  if delta<functional_acuity:
+    return("none")
+  elif beam_pos>stream_pos:
+    return("<<")
+  else:
+    return(">>")
 
 # The display and hit-counting logic are intertwined. Maybe they
 # shouldn't be. Pretty straight-forward refactoring would pull them
@@ -168,12 +204,11 @@ def showpos(stream_pos, beam_pos, show_f, cycle):
         print(f'] s:{stream_pos} b:{beam_pos} {msg}')
         msg=""
 
-def run(show_f,initial_ord=0): # _f is a flag
+button_distances = 10
+
+def run(show_f): # _f is a flag
   f = open("results/r"+str(time.time())+".tsv", "w")
-  global operator_response_delay, functional_acuity, n_crazy_ivans, default_reps, hits, misses
-  operator_response_delay=initial_ord
-  n_ord_values_to_try=40
-  ord_delta=1
+  global functional_acuity, n_crazy_ivans, default_reps, hits, misses, button_distances, button_distance
   if show_f:
     reps = 1
   else:
@@ -181,7 +216,8 @@ def run(show_f,initial_ord=0): # _f is a flag
   f.write(f"Functional Acuity = {functional_acuity} stream_shift_amount= {stream_shift_amount}, p_stream_shift={p_stream_shift}, beam_shift_amount={beam_shift_amount}, p_crazy_ivan={p_crazy_ivan}\n")
   print(f"Functional Acuity = {functional_acuity} stream_shift_amount= {stream_shift_amount}, p_stream_shift={p_stream_shift}, beam_shift_amount={beam_shift_amount}, p_crazy_ivan={p_crazy_ivan}")
   f.write("operator_response_delay\tmean\tsem\tn_crazy_ivans\n")
-  for p in range(n_ord_values_to_try):
+  for local_button_distance in range(button_distances):
+    button_distance = 4+local_button_distance
     n_crazy_ivans = 0 # These are counted over all reps and then the mean is display at the end
     results = []
     for rep in range(reps):
@@ -190,23 +226,12 @@ def run(show_f,initial_ord=0): # _f is a flag
       if show_f:
         print(f"============================================\nHits={hits}, Misses={misses}, Win fraction={frac}\n")
       results=results+[frac]
-    operator_response_delay=operator_response_delay+ord_delta
-    print(f"@ ord={operator_response_delay} mean hit fraction = {format(numpy.mean(results),'.2f')} [se={format(sem(results),'.2f')}], crazy_ivans/reps = {n_crazy_ivans/reps}")
-    f.write(f"{operator_response_delay}\t{format(numpy.mean(results),'.2f')}\t{format(sem(results),'.2f')}\t{n_crazy_ivans/reps}\n")
+    print(f"@ button_distance={button_distance} mean hit fraction = {format(numpy.mean(results),'.2f')} [se={format(sem(results),'.2f')}], crazy_ivans/reps = {n_crazy_ivans/reps}")
+    f.write(f"{button_distance}\t{format(numpy.mean(results),'.2f')}\t{format(sem(results),'.2f')}\t{n_crazy_ivans/reps}\n")
   f.close()
 
 # If display is true, we only do one rep and only allow it to run 1000 cycles
-# For testing with display code, you'll want to set this inital_ord to 2 or greater
 
-p_crazy_ivan=0.01
-run(False,initial_ord=2) 
-
-# p_crazy_ivan=0.000
-# physical_acuity=0.02
-# functional_acuity=0.01
-# run(False, initial_ord=2) 
-# functional_acuity=0.02
-# run(False, initial_ord=2) 
-# functional_acuity=0.03
-# run(False, initial_ord=2) 
+p_crazy_ivan=0.00 # 0.01 is good
+run(False) 
 
