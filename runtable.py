b8a0456dad98fef950853dfadc24674f7f0a41fb
Jonathan Segal <jonathannsegal@gmail.com>
2022-06-22 15:13:04 -0700

    Update

--- runtable.py
+++ runtable.py
@@ -1,36 +1,89 @@
+"""
+RunTable Generator Tool
+"""
+
 import csv
-import matplotlib.pyplot as plt
-import numpy as np
-from datetime import datetime
+from datetime import datetime, timedelta
 from os import walk
+import plotly.express as px
+import pandas as pd
 
-path = "run_tables"
-filenames = next(walk(path), (None, None, []))[2]
+# Set path with run csv files
+PATH = "run_tables"
 
-fig, axs = plt.subplots(len(filenames), sharex=True, figsize=(12, 6))
+# Get all files in given path
+filenames = next(walk(PATH), (None, None, []))[2]
+df = pd.DataFrame([])
 for x, file in enumerate(filenames):
-       axs[x].yaxis.set_ticks([])
-       axs[x].set_ylabel(file[:len(file) - 4])
-       with open(f"{path}/{file}",'r') as f:
-              reader = csv.reader(f)
-              headers = next(reader)
-              data = [{h:x for (h,x) in zip(headers,row)} for row in reader]
-       positions = []
-       thickness = []
-       for entry in data:
-              if "RunStart" in entry:
-                     positions.append([datetime.strptime(entry["RunStart"], '%b/%d/%Y %H:%M:%S')])
-              if "run start" in entry:
-                     positions.append([datetime.strptime(entry["run start"], '%b/%d/%Y %H:%M:%S')])
-              if "RunDuration" in entry:
-                     thickness.append(float(entry["RunDuration"]))
-              if "Duration of run" in entry:
-                     thickness.append(float(entry["Duration of run"]))
-       axs[x].eventplot(
-              positions = np.array(positions), 
-              orientation="horizontal",
-              linewidths=np.array(thickness),
-              colors=['C{}'.format(i) for i in range(len(positions))])
-axs[0].set_title('Run Tables')
-axs[len(filenames)-1].set_xlabel('Time ->')
-plt.show()
+# Read the CSV with headers
+    with open(f"{PATH}/{file}",'r', encoding="utf-8") as f:
+        reader = csv.reader(f)
+        headers = next(reader)
+        data = [{h:x for (h,x) in zip(headers,row)} for row in reader]
+    for entry in data:
+        start = datetime.strptime(
+            entry["RunStart"], '%b/%d/%Y %H:%M:%S') if "RunStart" in entry else datetime.strptime(
+            entry["run start"], '%b/%d/%Y %H:%M:%S')
+        duration = timedelta(
+            seconds=int(entry["RunDuration"].split('.')[0]),
+            milliseconds=int(entry["RunDuration"].split('.')[1])
+            ) if "RunDuration" in entry else timedelta(
+                seconds=int(entry["Duration of run"].split('.')[0]),
+                milliseconds=int(entry["Duration of run"].split('.')[1]))
+        Comment = entry["Comment"] if "Comment" in entry else entry["Comments"]
+        Run = entry["Run"]
+        df = pd.concat([df, pd.DataFrame.from_records([{
+            "Task":f"{file[:len(file) - 4]}",
+            "Start":f"{start}",
+            "Finish":f"{start + duration}",
+            "Resource":f"{file[:len(file) - 4]}",
+            "Run": f"Run: {Run}",
+            "Nevents": entry["N events"] if "N events" in entry else "",
+            "Ndamaged": entry["N damaged"] if "N damaged" in entry else "",
+            "ShutterIn": entry["ShutterIn"] if "ShutterIn" in entry else "",
+            "WaveplateAngle": entry["WaveplateAngle"] if "WaveplateAngle" in entry else "",
+            "GasCell-Pressure": entry["GasCell-Pressure"] if "GasCell-Pressure" in entry else 
+                                entry["GasCell Pressure"] if "GasCell Pressure" in entry else "",
+            "JungFrau-Z": entry["JungFrau-Z"] if "JungFrau-Z" in entry else
+                          entry["Junfrau Z"] if "Junfrau Z" in entry else "",
+            "Pinhole-X": entry["Pinhole-X"] if "Pinhole-X" in entry else
+                         entry["Pinhole X"] if "Pinhole X" in entry else "",
+            "Pinhole-Y": entry["Pinhole-Y"] if "Pinhole-Y" in entry else
+                         entry["Pinhole Y"] if "Pinhole Y" in entry else "",
+            "GasCell-X": entry["GasCell-X"] if "GasCell-X" in entry else
+                         entry["GasCell X"] if "GasCell X" in entry else "",
+            "GasCell-Y": entry["GasCell-Y"] if "GasCell-Y" in entry else
+                         entry["GasCell Y"] if "GasCell Y" in entry else "",
+            "GasCell-Z": entry["GasCell-Z"] if "GasCell-Z" in entry else "",
+            "GasCell Yaw": entry["GasCell Yaw"] if "GasCell Yaw" in entry else "",
+            "GasCell Pitch": entry["GasCell Pitch"] if "GasCell Pitch" in entry else "",
+            "Photonenergy": entry["Photon energy"] if "Photon energy" in entry else "",
+            "Comment": f"{Comment}"}])])
+fig = px.timeline(df,
+            x_start="Start",
+            x_end="Finish",
+            y="Task",
+            color="Resource",
+            hover_name="Run",
+            hover_data=["Nevents",
+            "Ndamaged","ShutterIn",
+            "WaveplateAngle","GasCell-Pressure",
+            "JungFrau-Z","Pinhole-X","Pinhole-Y",
+            "GasCell-X","GasCell-Y","GasCell-Z",
+            "GasCell Yaw","GasCell Pitch",
+            "Photonenergy","Comment"])
+fig.update_yaxes(autorange="reversed")
+fig.show()
+
+# Stepped
+# functional aquity
+# Add the experiemnt number to the graph number
+# Estimate efficiency
+# 6b(9320 FA = 1.5, CI = 0.01, ACI = 5%)
+# Multiplying by the amount of time the data is taken
+# Funciotal aquity is something we controll directly, a function of the UI and training
+
+
+# Take a point on the graph and multiply it by the time (Datapoints) on the run
+# 36 datapoints per second
+# fraction of hits multiply by 36
